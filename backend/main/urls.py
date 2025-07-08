c6c144210b2f15644a2d946924025d3098281557
Bartolini515 <bartolini0690@gmail.com>
2025-07-08 22:20:54 +0200

    First Commit

---
+++ backend/main/urls.py
@@ -0,0 +1,14 @@
+from rest_framework.routers import DefaultRouter
+from .views import *
+from django.conf import settings
+
+router = DefaultRouter(trailing_slash=True)
+router.register('timetable', TimetableViewset, basename='timetable')
+router.register('login', LoginViewset, basename='login')
+router.register('moderator_panel', ModeratorPanelViewset, basename='moderator_panel')
+router.register('account', AccountViewset, basename='account')
+router.register('tasks', TasksViewset, basename='tasks')
+router.register('schedule', ScheduleViewset, basename='schedule')
+urlpatterns = router.urls
+if not settings.DEBUG:
+    urlpatterns = [url for url in urlpatterns if url.name != 'api-root']
