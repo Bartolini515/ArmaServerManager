0e4c36b6c6f57568d46dbdf0c845b1e6baed3f53
Bartolini515 <bartolini0690@gmail.com>
2025-07-29 18:43:59 +0200

    Temporary prints

--- backend/main/steamcmd/mods_download.py
+++ backend/main/steamcmd/mods_download.py
@@ -98,8 +98,11 @@ def steamcmd_download(mod: str, appid: int, login: str, password: str, steamcmd_
     else:
         args.append(f'+workshop_download_item {appid} {int(mod)}')
     args.append("+quit")
+    
+    print(args)
 
     process = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    print(process)
     
     stream_process_output(process, log_callback if log_callback else None)
     
