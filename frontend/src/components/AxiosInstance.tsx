1428de0167a3d528df2229b2fc22483107fdf9fe
Bartolini515 <bartolini0690@gmail.com>
2025-08-26 14:33:26 +0200

    Fix AxiosInstance isDebug

--- frontend/src/components/AxiosInstance.tsx
+++ frontend/src/components/AxiosInstance.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const myBaseUrl = import.meta.env.VITE_BASE_URL || "http://127.0.0.1:8000/api/";
-const isDebug = import.meta.env.VITE_DEBUG === "true" || true;
+const isDebug = import.meta.env.VITE_DEBUG !== "false";
 
 const config = {
 	baseURL: myBaseUrl,
