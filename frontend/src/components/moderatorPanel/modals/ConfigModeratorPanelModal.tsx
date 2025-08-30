5661ab4baa05ccb1fba0f0a7acae597dca3f8e44
Bartolini515 <bartolini0690@gmail.com>
2025-08-30 13:06:17 +0200

    Fix error display

--- frontend/src/components/moderatorPanel/modals/ConfigModeratorPanelModal.tsx
+++ frontend/src/components/moderatorPanel/modals/ConfigModeratorPanelModal.tsx
@@ -99,7 +99,12 @@ export default function ConfigModeratorPanel(props: Props) {
 			})
 			.catch((error: any) => {
 				console.log(error);
-				setAlert(error.message, "error");
+				setAlert(
+					error.response.data.message
+						? error.response.data.message
+						: error.message,
+					"error"
+				);
 			});
 	};
 
