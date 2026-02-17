a52507107d08146d599f2d25bcda79a96048d007
Bartolini515 <bartolini0690@gmail.com>
2026-02-17 21:31:26 +0100

    Updated the config content in start files

--- backend/main/modpreset/start_files.py
+++ backend/main/modpreset/start_files.py
@@ -17,14 +17,67 @@ def generate_server_config(username: str, password: str, arma_path: str, log_cal
         passwordAdmin = "{password}";
         password = "{password}";
         hostname = "FOG - {username} Server";
-        maxPlayers = 128;
+        maxPlayers = 32;
         motd[] = {{"Witaj na serwerze FOG użytkownika {username}!"}};
+        BattlEye = 0;
         verifySignatures = 0;
         logFile = "server_{username}_console.log";
-        BattlEye = 0;
-        steamProtocolMaxDataSize = 12048;
+        allowedFilePatching = 0;
+        steamProtocolMaxDataSize = 16384;
         forcedDifficulty = "Custom";
         disableVoN = 1;
+        
+        class DifficultyPresets
+        {{
+            class CustomDifficulty
+            {{
+                    class Options
+                    {{
+                            /* Simulation */
+
+                            reducedDamage = 0;              // Reduced damage
+
+                            /* Situational awareness */
+
+                            groupIndicators = 0;    // Group indicators (0 = never, 1 = limited distance, 2 = always)
+                            friendlyTags = 0;               // Friendly name tags (0 = never, 1 = limited distance, 2 = always)
+                            enemyTags = 0;                  // Enemy name tags (0 = never, 1 = limited distance, 2 = always)
+                            detectedMines = 0;              // Detected mines (0 = never, 1 = limited distance, 2 = always)
+                            commands = 0;                   // Commands (0 = never, 1 = fade out, 2 = always)
+                            waypoints = 0;                  // Waypoints (0 = never, 1 = fade out, 2 = always)
+                            tacticalPing = 0;               // Tactical ping (0 = disabled, 1 = in 3D scene, 2 = on map, 3 = both)
+
+                            /* Personal awareness */
+
+                            weaponInfo = 2;                 // Weapon info (0 = never, 1 = fade out, 2 = always)
+                            stanceIndicator = 2;    // Stance indicator (0 = never, 1 = fade out, 2 = always)
+                            staminaBar = 0;                 // Stamina bar
+                            weaponCrosshair = 0;    // Weapon crosshair
+                            visionAid = 0;                  // Vision aid
+
+                            /* View */
+
+                            thirdPersonView = 0;    // 3rd person view (0 = disabled, 1 = enabled, 2 = enabled for vehicles only (Since  Arma 3 v1.99))
+                            cameraShake = 1;                // Camera shake
+
+                            /* Multiplayer */
+
+                            scoreTable = 0;                 // Score table
+                            deathMessages = 0;              // Killed by
+                            vonID = 0;                              // VoN ID
+
+                            /* Misc */
+
+                            mapContent = 0;                 // Extended map content
+                            autoReport = 0;                 // (former autoSpot) Automatic reporting of spotted enemies by players only. This doesn't have any effect on AIs.
+                            multipleSaves = 0;              // Multiple saves
+                    }};
+
+                    // aiLevelPreset defines AI skill level and is counted from 0 and can have following values: 0 (Low), 1 (Normal), 2 (High), 3 (Custom).
+                    // when 3 (Custom) is chosen, values of skill and precision are taken from the class CustomAILevel.
+                    aiLevelPreset = 2;
+            }};
+        }};
     """
     config_path = f"{arma_path}/server_{username}.cfg"
     with open(config_path, 'w') as config_file:
